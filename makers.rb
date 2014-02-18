f735bd188c3bb1f9ffced803ec57cb4b14486f0b
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-02-18 17:41:53 +0000

    More playing around with if, else, booleans

--- makers.rb
+++ makers.rb
@@ -1,7 +1,19 @@
 # Assign the string "Makers" to a variable and print it out #
 people = "Makers"
-puts "The people variable is currently:" + people
+puts "The people variable is currently: #{people}"
 
 # Change the variable to "Coders" and print it again #
 people = "Coders"
-puts "The people variable is currently:" + people
+puts "The people variable is currently: #{people}"
+
+age = nil
+if age.nil?
+	puts "I have no idea how old I am"
+else
+	puts "I'm #{age} years old"	
+end
+
+2 + 2 == 4 #=> true
+2 + 2 == 5 #=> false
+2.even? #=> true
+6.odd? #=> false
