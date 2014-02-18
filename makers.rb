ea04d988ba0ac6d0f7f5d000991859888f6c5480
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-02-18 17:12:55 +0000

    Comments added

--- makers.rb
+++ makers.rb
@@ -1,4 +1,7 @@
-people = " Makers"
-puts "The people variable is currently: " + people
+# Assign the string "Makers" to a variable and print it out #
+people = "Makers"
+puts "The people variable is currently:" + people
+
+# Change the variable to "Coders" and print it again #
 people = "Coders"
-puts "The people variable is currently: " + people
+puts "The people variable is currently:" + people
