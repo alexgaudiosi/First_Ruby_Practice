5bc4a854778c535f0b15c181495d9ae741c64c96
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-02-19 09:33:05 +0000

    Some extra script additions

--- hello.rb
+++ hello.rb
@@ -1,7 +1,25 @@
-def hello(name)
-	puts "Welcome to learning how to code #{name}"
+def hello(name, age)
+	puts "Welcome #{name}, #{age} is definitely not too old to learning how to code."
 end
 
-hello('rob')
-hello('jane')
-hello('pete')
+hello('rob', 30)
+hello('jane', 72)
+hello('pete', 55)
+
+def monthly_payment(salary_per_annum)
+	return salary_per_annum / 12
+end
+
+puts "If the salary is £30,000 per year,"
+puts "the monthly payment will be #{monthly_payment(30000)}"
+
+def odd_or_even(number)
+	if number.odd?
+		"odd"
+	else
+		"even"
+	end
+end
+
+puts odd_or_even(2)
+puts odd_or_even(5)
